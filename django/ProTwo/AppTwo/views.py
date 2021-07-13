d502450c60eb1aeff269838770b7f4e2e65fb8ec
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-07-13 15:50:38 -0700

    Added second template

--- django/ProTwo/AppTwo/views.py
+++ django/ProTwo/AppTwo/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("<em>My Second App</em> ")
+
+def help(request):
+  help_dict = {'help_insert':'HELP PAGE'}
+  return render(request, 'appTwo/help.html', context=help_dict)
